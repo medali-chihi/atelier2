ecb870b5552e70fb54ca254192754973ec2430ba
medali chihi <mohamedali.chihi@gmail.com.tn>
2024-10-23 12:19:13 +0100

    first commit

--- app/Http/Controllers/EtudiantController.php
+++ app/Http/Controllers/EtudiantController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Etudiant;
+use App\Models\classe;
 
 class EtudiantController extends Controller
 {
     public function index(){
        $liste = Etudiant::orderby('nom','asc')->get();
         return view('etudiant',compact('liste'));
+    }
+    public function create(){
+        $classes=Classe::all();
+        return view('create',compact('classes'));
+    }
+    public function store (Request $request){
+$request->validate([
+    'nom'=>'required',
+    'prenom'=>'required',
+    'classe_id'=>'required'
+]);  
+Etudiant::create($request->all());
+return redirect()->route('etudiant')
+                ->with('success','student created successfully.');
     }
 }
